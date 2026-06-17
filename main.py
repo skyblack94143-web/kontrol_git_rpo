42cff4bb35928c76c29f2cf38f3dd99cbb9ae24d
Джонатан Меер <dvdvovo650@gmail.com>
2026-06-17 10:24:17 +0300

    base: create git practice project

--- main.py
+++ main.py
@@ -3,33 +3,15 @@ def show_project_title():
 
 
 def show_student_info():
-    student_name = "Фамилия Имя"
-    group = "Название группы"
+    student_name = "Gavrilov Vlad"
+    group = "RPO/1"
     print(f"Student: {student_name}")
     print(f"Group: {group}")
-
-
-def show_git_topics():
-    topics = [
-        "README.md",
-        ".gitignore",
-        "commits",
-        "branches",
-        "merge",
-        "fetch",
-        "pull",
-        "conflicts",
-    ]
-
-    print("Practice topics:")
-    for topic in topics:
-        print(f"- {topic}")
 
 
 def main():
     show_project_title()
     show_student_info()
-    show_git_topics()
 
 
 if __name__ == "__main__":
